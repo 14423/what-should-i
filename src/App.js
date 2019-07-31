9de0ad208163953c8b00288ebc0196b6db355535
CORP\pfnu <Priyanka.fnu@epsilon.com>
2019-07-31 11:15:06 -0500

    updated changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Loginscreen1 from './Loginscreen1'
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<Loginscreen1 parentContext={this}/>);
+    this.setState({
+      loginPage:loginPage
+    })
+  }
+  render() {
+    return (
+        <div className="App">
+          {this.state.loginPage}
+          {this.state.uploadScreen}
+        </div>
+    );
+  }
 }
-
+const style = {
+  margin: 15,
+};
 export default App;
